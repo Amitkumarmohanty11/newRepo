6b3c20bdec5fc8a87c1190244222fba344601ff0
Amit Kumar Mohanty <105871364+Amitkumarmohanty11@users.noreply.github.com>
2022-07-07 20:30:21 +0530

    update problem12.js

--- problem12.js
+++ problem12.js
@@ -1,8 +1,24 @@
-let n=5;
- const revVlaue=() =>
- {
-    let myRevVal = n.toString().split("").reverse().join("");
-    console.log(myRevVal);
-
- }
- revVlaue();
+function isPalindrome(n)
+{
+    
+    var rev = 0;
+    for (var i = n; Math.trunc(i) > 0; i /= 10)
+    {
+        rev = ((rev*10) + (Math.trunc(i)%10));
+          
+        }
+    
+    
+    return (n==rev);
+}
+      
+function countPal(max)
+{
+    for (var i = max; i <=max; i++)
+    {
+        if(isPalindrome(i))
+        console.log(i+" " );
+       }
+}
+  
+    countPal(5);
